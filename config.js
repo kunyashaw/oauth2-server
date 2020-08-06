006c6112ca9e80e0d8710c09045a8d931d9f3d4a
kunyashaw <zhang_zhong_lei@163.com>
2020-08-06 11:00:35 +0800

    push config.js

---
+++ config.js
@@ -0,0 +1,14 @@
+var Config = {
+    // DCOSUI_URL: 'https://dc.njuics.cn',
+    // LOGINAUTH_URL: 'http://114.212.189.147:3000',
+    DCOSUI_URL: 'http://localhost:4200',
+    LOGINAUTH_URL: 'http://localhost:3000',
+    LOGINAUTH_PORT: 3000,
+    root: __dirname,
+    client_id: "dc-njuics-cn",
+    client_secret: "dcos-nap",
+  	mongodbConfig: "mongodb://localhost/dcos-auth",
+  	token_destination: ""
+}
+
+module.exports = Config;
